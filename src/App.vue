57da32db39d0d34d9c4f47ce0e2b820b5dd6a2cb
lukxnut <kristian2015ok@gmail.com>
2024-03-12 15:43:57 +0700

    Create SortingVisualizer component and call it in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
-<template>
-</template>
+<template></template>
 
 <script>
+import SortingVisualizer from "./components/SortingVisualizer.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-  }
-}
+    SortingVisualizer,
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
