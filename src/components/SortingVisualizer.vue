66213ca797066095996825093f994df3a067325b
lukxnut <kristian2015ok@gmail.com>
2024-03-12 16:48:33 +0700

    Create function to display an array in a bar chart

--- src/components/SortingVisualizer.vue
+++ src/components/SortingVisualizer.vue
@@ -1,5 +1,42 @@
-<template></template>
+<template>
+  <div class="array-container">
+    <div class="array-bar" v-for="n in arr" :style="{ height: n + 'px' }"></div>
+  </div>
+</template>
 
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
 
-<style scoped></style>
+const arr = ref([]);
+
+const resetArr = () => {
+  arr.value = [];
+  for (let i = 0; i < 100; i++) {
+    arr.value.push(randomIntFromInterval(5, 550));
+  }
+};
+
+const randomIntFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+onMounted(() => {
+  resetArr();
+});
+</script>
+
+<style scoped>
+.array-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.array-bar {
+  width: 10px;
+  display: inline-block;
+
+  background-color: #42b883;
+  margin: 0 1px;
+}
+</style>
