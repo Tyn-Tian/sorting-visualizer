5de1a9a4211ee52660c395b27cdb4127935cfe91
lukxnut <kristian2015ok@gmail.com>
2024-03-18 15:57:14 +0700

    Refactor bubbleSort function, delete variable that never read

--- src/composables/sortingAlgorithms.js
+++ src/composables/sortingAlgorithms.js
@@ -101,17 +101,13 @@ export function getSortingAlgorithms() {
   // Bubble Sort
   const bubbleSort = (array) => {
     const animations = [];
-    let isSwapped = false;
 
     for (let i = 0; i < array.length; i++) {
-      isSwapped = false;
-
       for (let j = 0; j < array.length - i - 1; j++) {
         animations.push([j, j + 1]);
         if (array[j] > array[j + 1]) {
           animations.push([j + 1, j]);
           [array[j], array[j + 1]] = [array[j + 1], array[j]];
-          isSwapped = true;
         }
       }
     }
